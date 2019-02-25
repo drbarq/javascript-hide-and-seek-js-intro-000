25d9a6352c2f5e5c04b1cc7209ad118e77c23167
Joe Tustin <j.tustin@gmail.com>
2019-02-25 19:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function nestedTarget() {
  function deepestChild() {
    var deep = document.getElementById('app').querySelector('#grand-node').querySelectorAll('div');
    var length = deep.length - 1
-   console.log(deep[length].innerHTML)
+  // console.log(deep[length].innerHTML)
    return deep[length].innerHTML
       }
 
