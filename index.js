f7f38edb1da06ed37be5816a8fcd8e3fa12efb81
Joe Tustin <j.tustin@gmail.com>
2019-02-25 20:00:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,8 +56,6 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-
-
   for (let i = 0; i < lis.length; i++) {
   //   var number = parseInt(lis[i].innerHTML, 10) + n)
     lis[i].innerHTML = (parseInt(lis[i].innerHTML, 10) + n)
